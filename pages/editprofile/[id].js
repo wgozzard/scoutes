1155956f4bc546c2dad003e2874708740c24946f
mdsaddamvol <mdsaddamvoltas@gmail.com>
2021-03-05 18:48:52 +0600

    add sup stong security control

--- pages/editprofile/[id].js
+++ pages/editprofile/[id].js
@@ -2,34 +2,51 @@ import React from 'react';
 import { UserForm } from '../../components/UserForm';
 import Link from 'next/link';
 import { connect } from 'react-redux';
+import { firestore } from '../../firebase/firebase-config';
 const App = ({ uid, user }) => {
+  if (uid === null) {
+    return (
+      <div style={{ width: '100%', height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        this account does not exists
+      </div>
+    );
+  }
   if (user.id !== uid) {
-    console.log('hh', user, uid);
     return (
       <div style={{ width: '100%', height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         You are not the owner of this account
       </div>
     );
+  } else {
+    return (
+      <div className="App">
+        {user && user.id === uid ? (
+          <UserForm uid={uid} />
+        ) : (
+          <div
+            style={{ width: '100%', height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+          >
+            please
+            <Link href="/signin">
+              <a style={{ margin: '0 10px' }}> sign in </a>
+            </Link>
+            frist
+          </div>
+        )}
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      {user && user.id === uid ? (
-        <UserForm uid={uid} />
-      ) : (
-        <div style={{ width: '100%', height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          please
-          <Link href="/signin">
-            <a style={{ margin: '0 10px' }}> sign in </a>
-          </Link>
-          frist
-        </div>
-      )}
-    </div>
-  );
 };
-App.getInitialProps = ({ query }) => {
+App.getInitialProps = async ({ query }) => {
   const { id } = query;
-  return { uid: id };
+  const profileRef = firestore.doc(`profiles/${id}`);
+  const snapshot = await profileRef.get();
+
+  let uuid = null;
+  if (snapshot.exists) {
+    uuid = id;
+  }
+  return { uid: uuid };
 };
 const mapStateToProps = state => {
   return {
